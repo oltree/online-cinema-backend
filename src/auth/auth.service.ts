f0c7218d9c8574f474b07ff6a447cbe68b6d3ae9
oltree <o.melex@bk.ru>
2023-07-04 20:44:45 +0300

    feat:added register and login methods and easy validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+  BadRequestException,
+} from '@nestjs/common';
 import { ModelType } from '@typegoose/typegoose/lib/types';
+import { genSalt, hash, compare } from 'bcryptjs';
 import { InjectModel } from 'nestjs-typegoose';
-import { UserModel } from 'src/user/user.model';
+
+import { AuthDto } from './dto/auth.dto';
+import { UserModel } from '../user/user.model';
 
 @Injectable()
 export class AuthService {
+  private readonly saltRounds = 10;
+
   constructor(
     @InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>
   ) {}
 
-  async register(dto: any) {
-    const newUser = new this.UserModel(dto);
+  async register(dto: AuthDto) {
+    const existingUser = await this.findByEmail(dto.email);
+
+    if (existingUser) {
+      throw new BadRequestException(
+        'User with this email is already in the system!'
+      );
+    }
+
+    const salt = await genSalt(this.saltRounds);
+
+    const newUser = new this.UserModel({
+      email: dto.email,
+      password: await hash(dto.password, salt),
+    });
 
     return newUser.save();
+  }
+
+  async login(dto: AuthDto) {
+    const user = await this.findByEmail(dto.email);
+
+    if (!user) {
+      throw new UnauthorizedException('User not found!');
+    }
+
+    const isValidPassword = await compare(dto.password, user.password);
+
+    if (!isValidPassword) {
+      throw new UnauthorizedException('Invalid password!');
+    }
+
+    return user;
+  }
+
+  async findByEmail(email: string) {
+    return this.UserModel.findOne({ email });
   }
 }
