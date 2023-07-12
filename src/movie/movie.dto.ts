19a2aab5677e42f7589ae06a1b566e1aaa18c4b3
oltree <o.melex@bk.ru>
2023-07-12 19:59:05 +0300

    feat(movie):added dto, module and model

---
+++ src/movie/movie.dto.ts
@@ -0,0 +1,52 @@
+import {
+  IsArray,
+  IsBoolean,
+  IsNumber,
+  IsObject,
+  IsString,
+} from 'class-validator';
+
+export class Parameters {
+  @IsNumber()
+  year: number;
+
+  @IsNumber()
+  duration: number;
+
+  @IsString()
+  country: string;
+}
+
+export class MovieDto {
+  @IsString()
+  poster: string;
+
+  @IsString()
+  bigPoster: string;
+
+  @IsString()
+  title: string;
+
+  @IsString()
+  description: string;
+
+  @IsString()
+  slug: string;
+
+  @IsString()
+  videoUrl: string;
+
+  @IsObject()
+  parameters?: Parameters;
+
+  @IsArray()
+  @IsString({ each: true })
+  genres: string[];
+
+  @IsArray()
+  @IsString({ each: true })
+  actors: string[];
+
+  @IsBoolean()
+  isSendTelegram?: boolean;
+}
---
+++ src/movie/movie.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { MovieController } from './movie.controller';
+import { MovieService } from './movie.service';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { MovieModel } from './movie.model';
+
+@Module({
+  controllers: [MovieController],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: MovieModel,
+        schemaOptions: {
+          collection: 'Movie',
+        },
+      },
+    ]),
+  ],
+  providers: [MovieService],
+})
+export class MovieModule {}
